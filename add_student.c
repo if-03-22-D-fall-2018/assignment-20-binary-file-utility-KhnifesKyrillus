b72847242bed213c6ca7fca2ccb9e0c80e8072fe
Khnifes Kyrillus <khnifes.kyrillus@gmail.com>
2019-06-05 21:11:53 +0200

    fixed add and display all students works

--- add_student.c
+++ add_student.c
@@ -8,32 +8,33 @@ int main(int argc, char const *argv[])
     char last_name[Length];
     int age;
 
+    //Eingabe
     printf("First Name: ");
-    scanf("%s",first_name);
+    scanf("%s", first_name);
 
     printf("Last Name: ");
-    scanf("%s",last_name);
+    scanf("%s", last_name);
 
     printf("Age: ");
-    scanf("%d",&age);
+    scanf("%d", &age);
 
     FILE* fd=fopen("student.dat","r+");
     int id;
 
     fseek(fd,0,SEEK_SET);
-    fwrite(&id,sizeof(int),1,fd);
+    fread(&id,sizeof(int),1,fd);
     fseek(fd,0,SEEK_END);
-    //Daten werden in die File geschrieben
+    
     fwrite(&id,sizeof(id),1,fd);
     fwrite(first_name,sizeof(first_name),1,fd);
     fwrite(last_name,sizeof(last_name),1,fd);
     fwrite(&age,sizeof(age),1,fd);
+
     fseek(fd,0,SEEK_SET);
-    //id wird erhöht und eingefügt
+
+    //erhöht id und fügt es in file ein
     id++;
     fwrite(&id,sizeof(int),1,fd);
     fclose(fd);
-  
-
     return 0;
 }
--- init.c
+++ init.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 int main(int argc, char const *argv[])
 {
     FILE* fd=fopen("student.dat","w");
     int id=1;
-    fwrite(&id,sizeof(id),1,fd);
+
+    fwrite(&id,sizeof(int),1,fd);
     fclose(fd);
     return 0;
 }
